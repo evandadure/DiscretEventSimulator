01751975c6760a4e5d9ab56b25164bc890655bd8
NanoClem <clemdecoop.pro@gmail.com>
2020-04-27 19:14:01 +0200

    people outside can't met twice a trip

--- app/Person.py
+++ app/Person.py
@@ -7,9 +7,9 @@ outsiders = []
 
 
 def getDifference(l1 = [], l2 = []):
-    """ Get unique elements in l1, but not in l2.
+    """ Get unique elements in l1 without the ones in common with l2.
     """
-    return list(set(l1).difference(l2))
+    return list(set(l1) - set(l2))
 
 
 
@@ -35,11 +35,14 @@ class Person(object):
         """
         """
         while True:
+            # WAITING A BIT BEFORE GOING OUTSIDE
+            yield self.env.timeout(random.randint(1, 3))
             # TRIP
             print("{} begins an outside trip at {}".format(self.id, self.env.now))
             yield self.env.process(self.go_out(self.trip_duration))
-            print("{} gets back at home at {}, {} people met".format(self.id, self.env.now, self.met))  ##TODO : refresh met list
+            print("{} gets back at home at {}, {} people met".format(self.id, self.env.now, [p.id for p in self.met]))  ##TODO : refresh met list
             # WAITING
+            self.met = []               # re-initialize met people
             yield self.env.timeout(3)
 
 
@@ -48,7 +51,7 @@ class Person(object):
         """
         # notify the exit in the global list
         global outsiders
-        outsiders.append(self)       
+        outsiders.append(self)
         # begining to meet people                
         yield self.env.process(self.meet_people(duration))
         outsiders.remove(self)
@@ -61,14 +64,14 @@ class Person(object):
         tmp, notmet = [], []
         p = None
 
-        for i in range(self.nb_meeting):
+        for i in range(self.nb_meeting): 
             if outsiders:
                 tmp = outsiders.copy()
-                tmp.remove(self)                                        # avoid self-meeting
-                notmet = getDifference(tmp, self.met)                   # get people not met yet
+                tmp.remove(self)                                     # avoid self-meeting
+                notmet = getDifference(tmp, self.met)                # get people not met yet
                 if notmet:
                     p = notmet[random.randint(0, len(notmet)-1)]      # choose someone randomly among not met people
-                    self.met.append(p.id)                             # add him to met people
+                    self.met.append(p)                                # add him to met people
                     print("{} meets {} at {}".format(self.id, p.id, self.env.now))
 
         yield self.env.timeout(duration)
@@ -79,7 +82,7 @@ if __name__ == "__main__":
     
     env = simpy.Environment()
     
-    for i in range(10):
+    for i in range(2):
         Person(env, i)
 
     env.run(until=10)
